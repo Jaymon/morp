383397994898df62ec52bc4fc1b55c02bba2f9bd
Jay Marcyes <jay@marcyes.com>
2024-01-31 23:50:24 -0700

    Checkpoint commit. I don't think I like my approach and I don't trust it would be reliable enough

--- morp/interface/postgres.py
+++ morp/interface/postgres.py
@@ -10,19 +10,44 @@ from .base import Interface
 
 
 class Postgres(Interface):
-    """Dropfile interface using local files as messages, great for quick
-    prototyping or passing messages from the frontend to the backend on the same
-    machine
+    """PostgreSQL interface using "FOR UPDATE SKIP LOCKED" to treat postgres
+    tables like a queue
+
+    https://github.com/Jaymon/morp/issues/18
     """
     _connection = None
+    """Will hold the postgres connection
+
+    https://www.psycopg.org/psycopg3/docs/api/connections.html#the-connection-class
+    """
 
     class Status(Enum):
+        """The values for the status field in each queue table
+        """
         NEW = "new"
+        """Rows will be first inserted into the table using this value"""
+
         PROCESSING = "processing"
+        """If a row has been pulled out for processing it will have this value,
+        if it doesn't have this value then it can be pulled out of the queue
+        for processing and its status field will be updated to this value"""
+
         RELEASED = "released"
+        """If a message was manually released it will contain this value and
+        count will be incremented"""
 
     @contextmanager
     def cursor(self, name, connection, **kwargs):
+        """Return a connection cursor
+
+        https://www.psycopg.org/psycopg3/docs/api/cursors.html
+
+        :param name: str, the queue name
+        :param connection: psycopg.Connection, the postgres connection
+        :param **kwargs:
+            - transaction: bool, if True start a tx
+        returns: psycopg.Cursor
+        """
         cursor = None
 
         try:
@@ -39,13 +64,11 @@ class Postgres(Interface):
             if cursor is not None:
                 cursor.close()
 
-#         except Exception:
-#             if cursor is not None:
-#                 cursor.close()
-# 
-#             raise
-
     def _connect(self, connection_config):
+        """Connect to the db
+
+        https://www.psycopg.org/psycopg3/docs/api/connections.html
+        """
         self._connection = psycopg.connect(
             dbname=connection_config.path.lstrip("/"),
             user=connection_config.username,
@@ -65,45 +88,64 @@ class Postgres(Interface):
         self._connection = None
 
     def _create_table(self, name, connection):
-        sql = "\n".join([
-            "CREATE TABLE IF NOT EXISTS \"{}\" (".format(name),
-            # https://www.postgresql.org/docs/current/functions-uuid.html
-            # after postgres 13 gen_random_uuid() is builtin
-            "  _id UUID DEFAULT gen_random_uuid(),",
-            "  body BYTEA,",
-            "  status TEXT,",
-            "  count INTEGER DEFAULT 1,",
-            "  valid TIMESTAMPTZ,",
-            "  _created TIMESTAMPTZ,",
-            "  _updated TIMESTAMPTZ",
-            ")"
-        ])
+        """Internal method that will create the queue table named `name` if it
+        doesn't already exist
 
-        with self.cursor(name, connection) as cursor:
-            cursor.execute(sql)
+        :param name: str, the queue name
+        :param connection: psycopg.Connection
+        """
+        sqls = [
+            "\n".join([
+                "CREATE TABLE IF NOT EXISTS \"{}\" (".format(name),
+                # https://www.postgresql.org/docs/current/functions-uuid.html
+                # after postgres 13 gen_random_uuid() is builtin
+                "  _id UUID DEFAULT gen_random_uuid(),",
+                "  body BYTEA,",
+                "  status TEXT,",
+                "  count INTEGER DEFAULT 1,",
+                "  valid TIMESTAMPTZ,",
+                "  _created TIMESTAMPTZ,",
+                "  _updated TIMESTAMPTZ",
+                ")"
+            ]),
+            "\n".join([
+                "CREATE INDEX IF NOT EXISTS \"{}_index\" ON \"{}\" (".format(
+                    name,
+                    name
+                ),
+                "  valid,",
+                "  status,",
+                "  _created",
+                ")"
+            ])
+        ]
+
+        with self.cursor(name, connection, transaction=True) as cursor:
+            for sql in sqls:
+                cursor.execute(sql)
 
     def _send(self, name, connection, body, **kwargs):
+        now = valid = Datetime()
+        if delay_seconds := kwargs.get('delay_seconds', 0):
+            valid += delay_seconds
+
+        sql = "\n".join([
+            "INSERT INTO \"{}\"".format(name),
+            "  (body, status, valid, _created, _updated)",
+            "VALUES",
+            "  (%s, %s, %s, %s, %s)",
+            "RETURNING _id"
+        ])
+
+        sql_vars = [
+            body,
+            self.Status.NEW.value,
+            valid,
+            now,
+            now
+        ]
+
         try:
-            now = valid = Datetime()
-            if delay_seconds := kwargs.get('delay_seconds', 0):
-                valid += delay_seconds
-
-            sql = "\n".join([
-                "INSERT INTO \"{}\"".format(name),
-                "  (body, status, valid, _created, _updated)",
-                "VALUES",
-                "  (%s, %s, %s, %s, %s)",
-                "RETURNING _id"
-            ])
-
-            sql_vars = [
-                body,
-                self.Status.NEW.value,
-                valid,
-                now,
-                now
-            ]
-
             with self.cursor(name, connection) as cursor:
                 cursor.execute(sql, sql_vars)
                 d = cursor.fetchone()
@@ -132,83 +174,48 @@ class Postgres(Interface):
         valid = Datetime()
         count = 0.0
 
-        with self.cursor(name, connection, transaction=True) as cursor:
-            while count <= timeout:
-                now = time.time_ns()
+        while count <= timeout:
+            now = time.time_ns()
 
-#                 sql = "\n".join([
-#                     "DELETE FROM \"{}\"".format(name),
-#                     "WHERE _id = (",
-#                     "  SELECT _id",
-#                     "  FROM \"{}\"".format(name),
-#                     "  WHERE status = %s AND valid <= %s",
-#                     "  ORDER BY _created ASC",
-#                     "  FOR UPDATE SKIP LOCKED",
-#                     "  LIMIT 1",
-#                     ")",
-#                     "RETURNING *",
-#                 ])
+            sql = "\n".join([
+                "UPDATE \"{}\"".format(name),
+                "SET status = %s",
+                "WHERE _id = (",
+                "  SELECT _id",
+                "  FROM \"{}\"".format(name),
+                "  WHERE valid <= %s",
+                "  AND status != %s",
+                "  ORDER BY _created ASC",
+                "  FOR UPDATE SKIP LOCKED",
+                "  LIMIT 1"
+                ")",
+                "RETURNING _id, body, status, count, valid, _created, _updated"
+            ])
 
+            sql_vars = [
+                self.Status.PROCESSING,
+                valid,
+                self.Status.PROCESSING
+            ]
 
-                sql = "\n".join([
-                    "UPDATE \"{}\"".format(name),
-                    "SET status = %s",
-                    "WHERE _id = (",
-                    "  SELECT _id",
-                    "  FROM \"{}\"".format(name),
-                    "  WHERE valid <= %s",
-                    "  AND status != %s",
-                    "  ORDER BY _created ASC",
-                    "  FOR UPDATE SKIP LOCKED",
-                    "  LIMIT 1"
-                    ")",
-                    "RETURNING *"
-                ])
+            try:
+                with self.cursor(name, connection) as cursor:
+                    cursor.execute(sql, sql_vars)
+                    raw = cursor.fetchone()
 
-#                 sql = "\n".join([
-#                     "SELECT",
-#                     "  _id,",
-#                     "  body,",
-#                     "  status,",
-#                     "  count,",
-#                     "  valid,",
-#                     "  _created,",
-#                     "  _updated",
-#                     "FROM \"{}\"".format(name),
-#                     "WHERE valid <= %s",
-#                     "AND status != %s",
-#                     "ORDER BY _created ASC",
-#                     "FOR UPDATE SKIP LOCKED",
-#                     "LIMIT 1"
-#                 ])
+            except psycopg.errors.UndefinedTable:
+                pass
 
-                try:
-                    with connection.transaction():
-                        cursor.execute(sql, [self.Status.PROCESSING, valid, self.Status.PROCESSING])
-                        raw = cursor.fetchone()
+            finally:
+                if raw:
+                    _id = raw["_id"]
+                    body = raw["body"]
+                    break
 
-                except psycopg.errors.UndefinedTable:
-                    pass
-
-                finally:
-                    if raw:
-                        _id = raw["_id"]
-                        body = raw["body"]
-
-#                         sql = "\n".join([
-#                             "UPDATE \"{}\"".format(name),
-#                             "SET status = %s",
-#                             "WHERE _id = %s"
-#                         ])
-# 
-#                         cursor.execute(sql, [self.Status.PROCESSING, _id])
-
-                        break
-
-                    else:
-                        count += 0.1
-                        if count < timeout:
-                            time.sleep(0.1)
+                else:
+                    count += 0.1
+                    if count < timeout:
+                        time.sleep(0.1)
 
         return _id, body, raw
 
@@ -218,46 +225,44 @@ class Postgres(Interface):
             cursor.execute(sql, [fields["_id"]])
 
     def _release(self, name, connection, fields, **kwargs):
-        delay_seconds = kwargs.get('delay_seconds', 0)
-        _id = fields["_id"]
+        if delay_seconds := kwargs.get('delay_seconds', 0):
+            sql = "\n".join([
+                "UPDATE \"{}\" SET".format(name),
+                "  status = %s,",
+                "  count = count + 1,",
+                "  valid = %s",
+                "WHERE _id = %s"
+            ])
+
+            sql_vars = [
+                self.Status.RELEASED.value,
+                Datetime() + delay_seconds,
+                fields["_id"]
+            ]
+
+        else:
+            sql = "\n".join([
+                "UPDATE \"{}\" SET".format(name),
+                "  status = %s,",
+                "  count = count + 1,",
+                "WHERE _id = %s"
+            ])
+
+            sql_vars = [
+                self.Status.RELEASED.value,
+                fields["_id"]
+            ]
 
         with self.cursor(name, connection) as cursor:
-            if delay_seconds:
-                valid = Datetime() + delay_seconds
-                sql = "\n".join([
-                    "UPDATE \"{}\" SET".format(name),
-                    "  status = %s,",
-                    "  count = count + 1,",
-                    "  valid = %s",
-                    "WHERE _id = %s"
-                ])
-
-                sql_vars = [
-                    self.Status.RELEASED.value,
-                    valid,
-                    _id
-                ]
-
-            else:
-                sql = "\n".join([
-                    "UPDATE \"{}\" SET".format(name),
-                    "  status = %s,",
-                    "  count = count + 1,",
-                    "WHERE _id = %s"
-                ])
-
-                sql_vars = [
-                    self.Status.RELEASED.value,
-                    _id
-                ]
-
             cursor.execute(sql, sql_vars)
 
     def _clear(self, name, connection, **kwargs):
+        sql = "DELETE FROM TABLE \"{}\" CASCADE".format(name)
         with self.cursor(name, connection) as cursor:
-            cursor.execute("DELETE FROM TABLE \"{}\" CASCADE".format(name))
+            cursor.execute(sql)
 
     def _delete(self, name, connection, **kwargs):
+        sql = "DROP TABLE IF EXISTS \"{}\" CASCADE".format(name)
         with self.cursor(name, connection) as cursor:
-            cursor.execute("DROP TABLE IF EXISTS \"{}\" CASCADE".format(name))
+            cursor.execute(sql)
 
